ef9b2d40ae3431025dd0a9659453177a78beaa97
Nishant <nishant301294@gmail.com>
2014-10-18 04:30:39 -0400

    Reinitialized

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,14 @@
+module ApplicationHelper
+<<<<<<< HEAD
+# Returns the full title on a per-page basis.
+def full_title(page_title)
+  base_title = "Ruby on Rails Tutorial Sample App"
+  if page_title.empty?
+    base_title
+  else
+    "#{base_title} | #{page_title}"
+  end
+end
+=======
+>>>>>>> 39909b4d7dea82bab9284f5c9df824c06dd3bbbd
+end
